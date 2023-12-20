84a2bd380113a88d33830d1715b859957b7a96ea
Nithin Vijayakumar <nithin2463@gmail.com>
2023-12-19 19:39:13 -0500

    update lambda

--- backend/lambda/handler.py
+++ backend/lambda/handler.py
@@ -1,7 +1,7 @@
-# import boto3
+import boto3
 
 
-# dynamodb = boto3.resource("dynamodb")
+
 
 # # Instantiate a table resource object without actually
 # # creating a DynamoDB table. Note that the attributes of this table
@@ -12,8 +12,21 @@
 # print(table.creation_date_time)
 
 import os
+import logging
+
+def get_env(env_var_name: str) -> str:
+    if env_var := os.getenv(env_var_name):
+        return env_var
+    else:
+        logging.error(f"{env_var_name} is needed")
+        raise ValueError(f"{env_var_name} is needed")
+        
+
 
 def handler(context, event):
-    print(f"{os.environ['TABLE_NAME']=}")
+    TABLE_NAME: str = get_env("TABLE_NAME")
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(TABLE_NAME)
+    print(table.creation_date_time)
     print(f"{context=} {event=}")
     return os.environ['TABLE_NAME']
